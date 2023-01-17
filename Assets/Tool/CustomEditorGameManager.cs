2b98f94e0e5259a1884454278cd17f4af6cf8c00
MoonLoaf <91465706+MoonLoaf@users.noreply.github.com>
2023-01-17 20:30:15 +0100

    Curstom UI working in play mode...

--- Assets/Tool/CustomEditorGameManager.cs
+++ Assets/Tool/CustomEditorGameManager.cs
@@ -1,18 +1,25 @@
 using UnityEditor;
 using UnityEngine.UIElements;
 using UnityEditor.UIElements;
+using UnityEngine;
 
 #if true
 
 [CustomEditor(typeof(GameManager))]
 public class CustomEditorGameManager : Editor
 {
-    public VisualTreeAsset UXML;
+    [SerializeField] private VisualTreeAsset UXML = default;
     
     public override VisualElement CreateInspectorGUI()
     {
+        UXML = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Tool/GameGonfig.uxml");
+        
         var root = new VisualElement();
         UXML.CloneTree(root);
+
+        var foldout = new Foldout() { viewDataKey = "GameConfigFullInspectorFoldout", text = "Full Inspector" };
+        InspectorElement.FillDefaultInspector(foldout, serializedObject, this);
+        root.Add(foldout);
 
         return root;
     }
